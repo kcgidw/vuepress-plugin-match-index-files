0a0823ddc6f8a6dc06688ffba7c1908644177f5e
kcgidw <kc.gideonw@gmail.com>
2020-04-02 22:21:04 -0700

    cleanup

--- index.js
+++ index.js
@@ -11,18 +11,19 @@ function getMdFiles(baseDir) {
 module.exports = function(options, ctx) {
 	ctx.sourceDir = ctx.sourceDir.split('\\').join('/');
 
+	// find .md files and generate path if filename matches directory name
 	const addPages = getMdFiles(ctx.sourceDir).reduce((res, absPath) => {
 		const relPath = absPath.replace(ctx.sourceDir, '');
 
 		const tokens = absPath.split('/');
 		const relTokens = relPath.split('/');
 
-		const endpoint = tokens[tokens.length - 1].replace('.md', '');
-		const parent = tokens[tokens.length - 2];
+		const filename = tokens[tokens.length - 1].replace('.md', '');
+		const dirname = tokens[tokens.length - 2];
 
-		if (endpoint.toLowerCase() === parent.toLowerCase()) {
+		if (filename.toLowerCase() === dirname.toLowerCase()) {
 			const base = ctx.base === '/' ? '' : ctx.base;
-			relTokens.pop(); // remove endpoint
+			relTokens.pop(); // remove endpoint file
 			res.push({
 				path: base + relTokens.join('/') + '/',
 				filePath: absPath,
